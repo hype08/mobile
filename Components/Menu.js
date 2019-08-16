7d559cf0143e21315af11b4ec2032cf65f20ef51
Henry <tarou.88@gmail.com>
2019-08-16 13:29:17 +0900

    animated spring and touchable opacity

--- Components/Menu.js
+++ Components/Menu.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import { Animated } from "react-native";
+import { Animated, TouchableOpacity } from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
 
 class Menu extends React.Component {
   state = {
@@ -13,10 +15,21 @@ class Menu extends React.Component {
     }).start();
   }
 
+  toggleMenu = () => {
+    Animated.spring(this.state.top, {
+      toValue: 900
+    }).start();
+  };
+
   render() {
     return (
       <AnimatedContainer style={{ top: this.state.top }}>
         <Cover />
+        <TouchableOpacity onPress={this.toggleMenu}>
+          <Closeview>
+            <Icon name="ios-close" size={44} color="#546bfb" />
+          </Closeview>
+        </TouchableOpacity>
         <Content />
       </AnimatedContainer>
     );
@@ -24,6 +37,17 @@ class Menu extends React.Component {
 }
 
 export default Menu;
+
+const Icon = styled(Ionicons)``;
+
+const Closeview = styled.View`
+  width: 44px;
+  height: 44px;
+  border-radius: 22px;
+  background: white;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Container = styled.View`
   position: absolute;
