6839985231cb12da714ed549cb6772ce77f17ccd
Henry <tarou.88@gmail.com>
2019-09-06 20:06:38 +0900

    TabNavigator as main Nav

--- navigator/TabNavigator.js
+++ navigator/TabNavigator.js
@@ -6,3 +6,24 @@ import {
 } from "react-navigation";
 import HomeScreen from "../screens/HomeScreen";
 import SectionScreen from "../screens/SectionScreen";
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  Section: SectionScreen
+});
+
+const CoursesStack = createStackNavigator({
+  Courses: SectionScreen
+});
+
+const ProjectsStack = createStackNavigator({
+  Projects: SectionScreen
+});
+
+const TabNavigator = createBottomTabNavigator({
+  HomeStack,
+  CoursesStack,
+  ProjectsStack
+});
+
+export default TabNavigator;
