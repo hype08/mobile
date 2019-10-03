ec0e8b2d320fa0db35685085e83f454aee98d2de
Henry <tarou.88@gmail.com>
2019-10-03 18:22:54 +0900

    graphql and apollo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,11 +10,47 @@ import {
 import styled from "styled-components";
 import { Ionicons } from "@expo/vector-icons";
 import { connect } from "react-redux";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
 
 import Card from "../components/Card";
 import Logo from "../components/Logo";
 import Course from "../components/Course";
 import Menu from "../components/Menu";
+import { Query } from "react-apollo";
+
+const CardsQuery = gql`
+  {
+    cardsCollection {
+      items {
+        title
+        subtitle
+        image {
+          title
+          description
+          contentType
+          fileName
+          size
+          url
+          width
+          height
+        }
+        subtitle
+        caption
+        logo {
+          title
+          description
+          contentType
+          fileName
+          size
+          url
+          width
+          height
+        }
+      }
+    }
+  }
+`;
 
 function mapStateToProps(state) {
   return { action: state.action };
@@ -117,24 +153,34 @@ class HomeScreen extends React.Component {
                 style={{ paddingBottom: 30 }}
                 showsHorizontalScrollIndicator={false}
               >
-                {cards.map((card, index) => (
-                  <TouchableOpacity
-                    key={index}
-                    onPress={() => {
-                      this.props.navigation.push("Section", {
-                        section: card
-                      });
-                    }}
-                  >
-                    <Card
-                      title={card.title}
-                      image={card.image}
-                      caption={card.caption}
-                      logo={card.logo}
-                      subtitle={card.subtitle}
-                    />
-                  </TouchableOpacity>
-                ))}
+                <Query query={CardsQuery}>
+                  {({ loading, error, data }) => {
+                    if (loading) return <Message>Loading...</Message>;
+                    if (error) return <Message>Error...</Message>;
+                    return (
+                      <CardsContainer>
+                        {data.cardsCollection.items.map((card, index) => (
+                          <TouchableOpacity
+                            key={index}
+                            onPress={() => {
+                              this.props.navigation.push("Section", {
+                                section: card
+                              });
+                            }}
+                          >
+                            <Card
+                              title={card.title}
+                              image={card.image}
+                              caption={card.caption}
+                              logo={card.logo}
+                              subtitle={card.subtitle}
+                            />
+                          </TouchableOpacity>
+                        ))}
+                      </CardsContainer>
+                    );
+                  }}
+                </Query>
               </ScrollView>
               <Subtitle>Popular</Subtitle>
               {courses.map((course, index) => (
@@ -161,6 +207,17 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(HomeScreen);
+
+const Message = styled.Text`
+  margin: 20px;
+  color: #b8bece;
+  font-size: 15px;
+  font-weight: 500;
+`;
+
+const CardsContainer = styled.View`
+  flex-direction: row;
+`;
 
 const RootView = styled.View`
   background: black;
@@ -215,36 +272,35 @@ const TitleBar = styled.View`
 
 const logos = [
   {
-    image: require("../assets/logo-framerx.png")
-    // text: "Framer X"
+    image: require("../assets/logo-framerx.png"),
+    text: "Framer X"
   },
   {
-    image: require("../assets/logo-figma.png")
-    // text: "Figma"
+    image: require("../assets/logo-figma.png"),
+    text: "Figma"
   },
   {
-    image: require("../assets/logo-studio.png")
-    // text: "Studio"
+    image: require("../assets/logo-studio.png"),
+    text: "Studio"
   },
   {
-    image: require("../assets/logo-react.png")
-    // text: "React"
+    image: require("../assets/logo-react.png"),
+    text: "React"
   },
   {
-    image: require("../assets/logo-swift.png")
-    // text: "Swift"
+    text: "Swift"
   },
   {
-    image: require("../assets/logo-sketch.png")
-    // text: "Sketch"
+    image: require("../assets/logo-sketch.png"),
+    text: "Sketch"
   }
 ];
 
 const cards = [
   {
-    // title: "React Native for Designers",
+    title: "React Native for Designers",
     image: require("../assets/background11.jpg"),
-    // subtitle: "React Native",
+    subtitle: "React Native",
     caption: "1 of 12 sections",
     logo: require("../assets/logo-react.png")
   },
@@ -256,16 +312,16 @@ const cards = [
     logo: require("../assets/logo-react.png")
   },
   {
-    // title: "Props and Icons",
+    title: "Props and Icons",
     image: require("../assets/background13.jpg"),
-    // subtitle: "React Native",
+    subtitle: "React Native",
     caption: "3 of 12 sections",
     logo: require("../assets/logo-react.png")
   },
   {
-    // title: "Static Data and Loop",
+    title: "Static Data and Loop",
     image: require("../assets/background14.jpg"),
-    // subtitle: "React Native",
+    subtitle: "React Native",
     caption: "4 of 12 sections",
     logo: require("../assets/logo-react.png")
   }
@@ -273,40 +329,40 @@ const cards = [
 
 const courses = [
   {
-    // title: "Prototype in InVision Studio",
+    title: "Prototype in InVision Studio",
     subtitle: "10 sections",
     image: require("../assets/background13.jpg"),
     logo: require("../assets/logo-studio.png"),
     author: "Henry",
-    avatar: require("../assets/avatar.jpg")
-    // caption: "Design an interactive prototype"
+    avatar: require("../assets/avatar.jpg"),
+    caption: "Design an interactive prototype"
   },
   {
-    // title: "React for Designers",
+    title: "React for Designers",
     subtitle: "12 sections",
     image: require("../assets/background11.jpg"),
     logo: require("../assets/logo-react.png"),
     author: "Henry",
-    avatar: require("../assets/avatar.jpg")
-    // caption: "Learn to design and code a React site"
+    avatar: require("../assets/avatar.jpg"),
+    caption: "Learn to design and code a React site"
   },
   {
-    // title: "Design and Code with Framer X",
+    title: "Design and Code with Framer X",
     subtitle: "10 sections",
     image: require("../assets/background14.jpg"),
     logo: require("../assets/logo-framerx.png"),
     author: "Henry",
-    avatar: require("../assets/avatar.jpg")
-    // caption: "Create powerful design and code components for your app"
+    avatar: require("../assets/avatar.jpg"),
+    caption: "Create powerful design and code components for your app"
   },
   {
-    // title: "Design System in Figma",
+    title: "Design System in Figma",
     subtitle: "10 sections",
     image: require("../assets/background6.jpg"),
     logo: require("../assets/logo-figma.png"),
     author: "Henry",
-    avatar: require("../assets/avatar.jpg")
-    // caption:
-    //   "Complete guide to designing a site using a collaborative design tool"
+    avatar: require("../assets/avatar.jpg"),
+    caption:
+      "Complete guide to designing a site using a collaborative design tool"
   }
 ];
